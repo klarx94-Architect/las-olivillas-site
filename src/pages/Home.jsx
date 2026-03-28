4bb08848b0bbaaec76cc17d1d6de07e02476eb1b
klarx94-Architect <klarx94@example.com>
2026-03-28 18:19:59 +0100

    FixLinkImport

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../components/home/Hero';
 import InteractiveMenu from '../components/home/InteractiveMenu';
 import ServicesSection from '../components/home/ServicesSection';
